0a831e8f126801d59d4e57a281250a788f86c7e3
MarceEspiGmz <espinalgomezclaudiamarcela45@gmail.com>
2024-09-09 20:29:41 -0500

    Cambios

---
+++ eje2.py
@@ -0,0 +1,8 @@
+anio_actual = int(input('introdce el anio actal:'))
+anio_referencia = int(input('introduce el anio de referecia:'))
+difference = anio_referencia-anio_actual
+if  < 0:
+   
+elif diferencia < 0:
+else:
+    print('estamos en el anio[anio_referencia]:')
---
+++ eje3.py
@@ -0,0 +1,9 @@
+num1 = float(input('introduce el primer numero:'))
+num2 = float(input('introduce el segundo numero:')) 
+num3 = float(input('introduce el tercer nmero'))
+if num1 == num2 == num3:
+    print('los tres nmeros son iguales.')
+elif num1 == num2 or num1 == num3 or nume2 ==num3:
+    print('hay dos numeros iguales.')
+else:
+    print('los tres numeros son distintos.')
